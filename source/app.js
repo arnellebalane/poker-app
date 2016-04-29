5ca280660075e2911f555e12947ce9fb8c3da2a2
Arnelle Balane <arnellebalane@gmail.com>
2016-04-30 05:14:33 +0800

    move routes to app.js

--- source/app.js
+++ source/app.js
@@ -1,9 +1,11 @@
 var path = require('path');
 var express = require('express');
 var nunjucks = require('nunjucks');
+var datastore = require('./datastore');
 
 
 var app = express();
+var users = datastore('users');
 
 app.set('PORT', process.env.PORT || 3000);
 app.set('TEMPLATES_DIRECTORY', path.join(__dirname, 'templates'));
@@ -21,7 +23,9 @@ app.use('/static', express.static(app.get('STATIC_DIRECTORY')));
 
 
 app.get('/', function(request, response) {
-    response.render('index.html', {});
+    users.all().then(function(entities) {
+        response.render('index.html', { users: entities });
+    });
 });
 
 
