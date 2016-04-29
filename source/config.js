0a369733660b17bf032592130abdc6b49e98b472
Arnelle Balane <arnellebalane@gmail.com>
2016-04-30 04:48:23 +0800

    make config.js read config.json for config variables

--- source/config.js
+++ source/config.js
@@ -1,1 +1,10 @@
-exports.GCLOUD_PROJECT = '';
+var fs = require('fs');
+var path = require('path');
+
+
+var configPath = path.join(__dirname, '..', 'config.json');
+var config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+Object.keys(config).forEach(function(variable) {
+    exports[variable] = config[variable];
+});
